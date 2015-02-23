0e29ef9d0252f9bdbba4d43f467f66886c7ef245
Richard Shade <rshade@rightscale.com>
2015-02-23 17:39:27 -0500

    adding default

--- recipes/aws.rb
+++ recipes/aws.rb
@@ -1,5 +1,4 @@
 node.default['rsc_dns']['dns_provider']='aws'
-node.default['rsc_dns']['provider']='dns_default'
 node.default['rsc_dns']['credentials'] = {
   'aws_access_key_id' => node['rsc_dns']['access_key'],
   'aws_secret_access_key' => node['rsc_dns']['secret_access_key']
