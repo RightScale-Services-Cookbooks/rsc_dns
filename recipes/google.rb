aa3343734dc044a80e1ed782cb9c1fe6d41a5985
Richard Shade <rshade@rightscale.com>
2015-02-25 14:57:45 -0600

    adding tests

--- recipes/google.rb
+++ recipes/google.rb
@@ -1,8 +1,2 @@
-
-log "not implemented yet"
-
-#node.default['rsc_dns']['dns_provider']='google'
-#node.default['rsc_dns']['credentials'] = {
-#  'dnsmadeeasy_api_key' => node['rsc_dns']['user'],
-#  'dnsmadeeasy_secret_key' => node['rsc_dns']['password']
-#}
+include_recipe "dns::fog"
+node.default['rsc_dns']['dns_provider']='google'
