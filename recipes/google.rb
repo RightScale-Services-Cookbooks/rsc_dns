b921bfb442552c28f0f6d59415d260e0bf00fe83
Richard Shade <rshade@rightscale.com>
2015-02-25 15:12:51 -0600

    adding keys

--- recipes/google.rb
+++ recipes/google.rb
@@ -1,2 +1,7 @@
-include_recipe "dns::fog"
+include_recipe 'dns::fog'
 node.default['rsc_dns']['dns_provider']='google'
+node.default['rsc_dns']['credentials'] = {
+  'google_client_email' => node['rsc_dns']['access_key'],
+  'google_key_location' => "#{Chef::Config[:file_cache_path]}/key.pk12"
+  'google_key_string' => node['rsc_dns']['secret_access_key']
+}
